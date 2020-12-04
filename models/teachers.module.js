8fe603a46de6d9ffeadf3e9c14f9f137602bc587
lethanhvietctt5 <lethanhviet7c@gmail.com>
2020-12-04 10:49:10 +0700

    schedule + list class for actor GV

--- models/teachers.module.js
+++ models/teachers.module.js
@@ -10,5 +10,15 @@ module.exports = {
 
     getAllAccounts() {
         return db.getAll(TABLE_ACCOUNT_TEACHER);
-    }
+    },
+
+    getSchedule(id_gv) {
+        return db.query(`SELECT TEN_HP, PHONG_HOC, TEN_LOP, TIET_BAT_DAU, TIET_KET_THUC FROM LOP_HOC JOIN HOC_PHAN 
+        WHERE HOC_PHAN.MA_HP = LOP_HOC.MA_HP AND ID_GIAO_VIEN = '${id_gv}'`);
+    },
+
+    getListClass(id_gv) {
+        return db.query(`SELECT ID_LOP_HOC, TEN_LOP, HOC_PHAN.MA_HP, TEN_HP FROM LOP_HOC JOIN HOC_PHAN 
+        WHERE HOC_PHAN.MA_HP = LOP_HOC.MA_HP AND ID_GIAO_VIEN = '${id_gv}'`)
+    },
 }
--- routes/teachers.route.js
+++ routes/teachers.route.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const teachersModel = require('../models/teachers.module');
+const bcrypt = require('bcrypt');
 
 router.get('/', async (req, res) => {
+    const data = await teachersModel.getAllAccounts();
+    // data.forEach((account) => {
+    //     bcrypt.hash('123456789', 10, (e, hash) => {
+    //         db.query(`UPDATE ACCOUNT_GIAO_VIEN SET MAT_KHAU = '${hash}' WHERE ID_GIAO_VIEN = '${account['ID_GIAO_VIEN']}'`);
+    //     })
+    // })
     res.render('teachers/teachers');
+})
+
+router.get('/scheduler', async (req, res) => {
+    const data = await teachersModel.getSchedule(req.session.username);
+    console.log(data);
+    res.render('teachers/schedule', {
+        isSchedule: true,
+        schedule: data,
+    });
+})
+
+router.get('/list_class', async (req, res) => {
+    const data = await teachersModel.getListClass(req.session.username);
+    console.log(data);
+    res.render('teachers/listClass', {
+        isListClass: true,
+        listClass: data,
+    })
 })
 
 module.exports = router;
