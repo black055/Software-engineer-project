154b5e406c9bb800202a76e1a980e370c3bff6e4
Le Thanh Viet <lethanhviet7c@gmail.com>
2020-12-18 13:47:59 +0700

    config for deploying

--- utils/db.js
+++ utils/db.js
@@ -2,13 +2,14 @@ const mysql = require('mysql');
 const util = require('util');
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'sql10.freemysqlhosting.net',
     port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'cnpm',
-    connectionLimit: 50,
+    user: 'sql10382784',
+    password: 'jUdRJjIDmU',
+    database: 'sql10382784',
+    connectionLimit: 100,
     dateStrings: ['DATE','DATETIME'],
+    charset : 'utf8mb4'
 });
 
 const pool_query = util.promisify(pool.query).bind(pool);
